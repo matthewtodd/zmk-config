d5a422c8d723be2b5b0542c19c0ae68c35b83fea
Matthew Todd <matthew@matthewtodd.org>
2026-04-26 16:18:42 -0400

    Rename & remove the output

--- share/fish/vendor_conf.d/zmk-config-activate.fish
+++ share/fish/vendor_conf.d/zmk-config-activate.fish
@@ -1,14 +1,13 @@
 set -g __zmk_config__ (realpath (status dirname)/../../..)
 
-function __build_activate__ --on-variable PWD
+function __zmk_config_activate__ --on-variable PWD
   if test (pwd) = $__zmk_config__
-    fish_add_path -gv $__zmk_config__/bin
+    fish_add_path -g $__zmk_config__/bin
   else
     if set -l index (contains -i -- $__zmk_config__/bin $fish_user_paths)
       set -e fish_user_paths[$index]
-      echo set -g fish_user_paths $fish_user_paths
     end
   end
 end
 
-__build_activate__
+__zmk_config_activate__
