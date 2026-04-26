9e915c7f21cda12c415059568e0c7cfae5893d21
Matthew Todd <matthew@matthewtodd.org>
2026-04-26 16:20:51 -0400

    Tidy

--- share/fish/vendor_conf.d/zmk-config-activate.fish
+++ share/fish/vendor_conf.d/zmk-config-activate.fish
@@ -3,10 +3,8 @@ set -g __zmk_config__ (realpath (status dirname)/../../..)
 function __zmk_config_activate__ --on-variable PWD
   if test (pwd) = $__zmk_config__
     fish_add_path -g $__zmk_config__/bin
-  else
-    if set -l index (contains -i -- $__zmk_config__/bin $fish_user_paths)
-      set -e fish_user_paths[$index]
-    end
+  else if set -l index (contains -i -- $__zmk_config__/bin $fish_user_paths)
+    set -e fish_user_paths[$index]
   end
 end
 
